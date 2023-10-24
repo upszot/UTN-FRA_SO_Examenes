4203c69f0c43662dda12beaceee5ae16c780d3d1
upszot <upszot@gmail.com>
2023-10-24 01:51:31 -0300

    listo punto 2

--- 202310/resolucion/punto2.sh
+++ 202310/resolucion/punto2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./punto2_consigna.sh
+./UTN-FRA_SO_Examenes/202310/resolucion/punto2_consigna.sh
 echo 
 echo "--- RESOLUCION PUNTO 2 ---"
 echo
@@ -33,7 +33,6 @@ echo "Particionamiento del resto del disco tipo LVM(8e) "
 sudo fdisk $DISCO << EOF
 n
 p
-4
 
 
 t
@@ -45,6 +44,16 @@ EOF
 echo
 sudo fdisk -l $DISCO
 echo
-
+echo "Creo los pv, vg y lv"
+sudo pvcreate ${DISCO}3 ${DISCO}4
+sudo vgcreate vg_docker ${DISCO}3
+sudo vgcreate vg_datos ${DISCO}4
+lvcreate -l +100%FREE vg_docker -n lv_docker
+lvcreate -L +1G vg_datos -n lv_multimedia
+lvcreate -l +100%FREE vg_datos -n lv_repo
+echo
+echo "Muestro como quedaron los pv, vg y lv"
+sudo pvs; sudo vgs; sudo lvs
+echo 
 
 
