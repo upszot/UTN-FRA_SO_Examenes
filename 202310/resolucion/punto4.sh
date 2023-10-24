96e5f5164a83b113b465b05436bce07db89363c8
upszot <upszot@gmail.com>
2023-10-24 03:14:17 -0300

    add - instalacion de tree en el punto 4

--- 202310/resolucion/punto4.sh
+++ 202310/resolucion/punto4.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+echo "instalo tree para tenerlo a mano"
+sudo apt update && sudo apt install tree -y
+echo
 echo "Punto 4 - Creacion de estructura simetrica "
 
 mkdir -p Estructura_1/{Alumnos,profesores}/materias_{1..50}
 echo
 echo "Muestro la estrucura"
-tree Estructura_1 
+tree $HOME/Estructura_1 
 
--- 202310/resolucion/punto5.sh
+++ 202310/resolucion/punto5.sh
@@ -5,5 +5,5 @@ echo "Punto 5 - Creacion de estructura Asimetrica "
 mkdir -p Estructura_2/{Aves/{Camina/Gallina,Nada/Pinguino,Vuelan/{Halcon,Paloma}},Mamiferos/{Camina/{Gato,Perro},Nada/{Ballena,Delfin}}}
 echo
 echo "Muestro la estrucura"
-tree Estructura_2 
+tree $HOME/Estructura_2 
 
