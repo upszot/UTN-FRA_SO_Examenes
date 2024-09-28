d18c08c01adcf8dca56b68890e2adae38b2156e2
upszot <upszot@gmail.com>
2024-09-28 19:27:26 -0300

    FIX: Discover path repo alumno, genera estructura

--- 202410/script_Precondicion.sh
+++ 202410/script_Precondicion.sh
@@ -3,14 +3,22 @@ TIMESTAMP=$(date +%Y%m%d)
 
 REPO_ALUMNO=$(find $HOME -name UTNFRA_SO_1P2C_2024*)
 
-mkdir ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_A.sh
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_B.sh
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_C.sh
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_D.sh
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_E.sh
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_F.sh
-touch ${REPO_ALUMNO}/RTA_Examen_${TIMESTAMP}/Punto_G.sh
+if [ -z $REPO_ALUMNO ]; then
+
+    echo "Por Favor antes de ejecutar este script"
+    echo "clone su repositorio dentro de $HOME/repogit/"
+    exit 1
+fi
+
+mkdir ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_A.sh
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_B.sh
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_C.sh
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_D.sh
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_E.sh
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_F.sh
+touch ${REPO_ALUMNO}/RTA_SCRIPT_Examen_${TIMESTAMP}/Punto_G.sh
+mkdir ${REPO_ALUMNO}/RTA_ARCHIVOS_Examen_${TIMESTAMP}
 
 
 # Habilita la opción para agregar nuevos comandos al final del historial de comandos.
@@ -22,6 +30,7 @@ sudo chattr +a ~/.bash_history
 
 # Agrega configuración al final del archivo .bashrc
 cat << EOF >> ~/.bashrc
+
 ###########################################################
 # Configuración del historial de comandos #
 ###########################################################
@@ -31,6 +40,7 @@ export HISTSIZE=10000
 export HISTFILESIZE=-1
 # Actualiza y sincroniza el historial de comandos entre sesiones.
 export PROMPT_COMMAND="history -a; history -c; history -r; \$PROMPT_COMMAND"
+
 EOF
 # Recarga el archivo .bashrc para aplicar los cambios.
 . ~/.bashrc
