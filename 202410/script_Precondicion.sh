d1a990cf16fb7b05b6b3bfc4d698a41e9bd01b6f
upszot <upszot@gmail.com>
2024-09-28 20:17:45 -0300

    FIX: Discover path repo alumno, genera estructura

--- 202410/script_Precondicion.sh
+++ 202410/script_Precondicion.sh
@@ -31,16 +31,16 @@ if ! grep -cq "Configuración del historial de comandos"  ~/.bashrc ; then
     # Agrega configuración al final del archivo .bashrc
     cat << EOF >> ~/.bashrc
 
-    ###########################################################
-    #     Configuración del historial de comandos 
-    #---------------------------------------------------------#
-    # Establece el tamaño máximo del historial en 10000 comandos.
-    export HISTSIZE=10000
-    # No hay límite en el tamaño del archivo de historial.
-    export HISTFILESIZE=-1
-    # Actualiza y sincroniza el historial de comandos entre sesiones.
-    export PROMPT_COMMAND="history -a; history -c; history -r; \$PROMPT_COMMAND"
-    ###########################################################
+###########################################################
+#     Configuración del historial de comandos 
+#---------------------------------------------------------#
+# Establece el tamaño máximo del historial en 10000 comandos.
+export HISTSIZE=10000
+# No hay límite en el tamaño del archivo de historial.
+export HISTFILESIZE=-1
+# Actualiza y sincroniza el historial de comandos entre sesiones.
+export PROMPT_COMMAND="history -a; history -c; history -r; \$PROMPT_COMMAND"
+###########################################################
 
 EOF
 fi
