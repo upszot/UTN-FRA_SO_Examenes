75ab52d233b1ea74740a125544e7f377e99a66c4
upszot <upszot@gmail.com>
2024-10-08 00:30:33 -0300

    FIX: arreglado

--- 202410/_checks/check_C.sh
+++ 202410/_checks/check_C.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+PARCIAL="UTNFRA_SO_1P2C_2024"
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-MAPEO_FILE_PRINCIPAL="$SCRIPT_DIR/.soporte/Punto_A_Mapeo.txt"
-PATH_PRINCIPAL="$(head -n1 $MAPEO_FILE_PRINCIPAL)"
+MAPEO_FILE_PRINCIPAL="$SCRIPT_DIR/.soporte/Punto_A_Mapeo.e"
+gpg --batch --yes --passphrase "$PARCIAL" --output ${MAPEO_FILE_PRINCIPAL}.txt -d $MAPEO_FILE_PRINCIPAL > /dev/null 2>&1
+PATH_PRINCIPAL="$(head -n1 ${MAPEO_FILE_PRINCIPAL}.txt)"
 
-MAPEO_FILE="$SCRIPT_DIR/.soporte/Punto_C_Mapeo.txt"
+MAPEO_FILE="$SCRIPT_DIR/.soporte/Punto_C_Mapeo.e"
+gpg --batch --yes --passphrase "$PARCIAL" --output ${MAPEO_FILE}.txt -d $MAPEO_FILE > /dev/null 2>&1
+
 
 
 PATH_TEMP="/tmp/borrar"
@@ -14,11 +18,10 @@ mkdir -p "$PATH_TEMP"
 sudo find $PATH_PRINCIPAL -type d | grep -v "lost+found" | sort | xargs -I PATH sudo stat -c %a,%U,%G PATH  > "$PATH_TEMP/Punto_C_Mapeo_Alumno.txt"
 
 # Comparar el resultado con el archivo de soporte
-if diff "$PATH_TEMP/Punto_C_Mapeo_Alumno.txt" "$MAPEO_FILE" > /dev/null; then
+if diff "$PATH_TEMP/Punto_C_Mapeo_Alumno.txt" "${MAPEO_FILE}.txt" > /dev/null; then
     echo "Punto C - OK"
 else
     echo "Punto C - Error Code: 1 - Mensaje: Permisos y/o Owner mal seteados"
-    diff "$PATH_TEMP/Punto_C_Mapeo_Alumno.txt" "$MAPEO_FILE"
+    diff "$PATH_TEMP/Punto_C_Mapeo_Alumno.txt" "${MAPEO_FILE}.txt"
     exit 1
 fi
-
